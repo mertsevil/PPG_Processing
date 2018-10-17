19f404d481bba327aa93eb9e859aaacafdec775f
rezaaskary <44175575+rezaaskary@users.noreply.github.com>
2018-10-17 18:07:25 -0500

    data

--- Code/main.m
+++ Code/main.m
@@ -6,9 +6,13 @@ NBVP=normalize(NBVP,-1,1);
 [re,im]=FFT(NBVP);
 % re(1:50)=0;
 % im(1:50)=0;
-re(600:5000)=0;
-im(600:5000)=0;
-
+re(550:5000)=0;
+im(550:5000)=0;
+valuex=normalize(valuex(1:5000),-1/3^0.5,1/3^0.5);
+valuey=normalize(valuey(1:5000),-1/3^0.5,1/3^0.5);
+valuez=normalize(valuez(1:5000),-1/3^0.5,1/3^0.5);
+NORM=sqrt(valuex.^2+valuey.^2+valuez.^2);
+% NORM=NORM(1:5000);
 N=max(size(re));
 for n=1:N
  p=0;
@@ -31,7 +35,8 @@ plot(NBVP,'linewidth',1.5)
 hold on
 % figure
 plot(x,'linewidth',1.5)
-% hold on
+hold on
+plot(NORM,'linewidth',1.5)
 figure
 fer_magplot(NBVP,32,'normalized','dB')
 hold on
